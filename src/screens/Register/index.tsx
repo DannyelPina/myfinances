bc60a7454607817baadc60ef351cbf22e964e827
DannyelPina <nataniel06@live.com.pt>
2022-01-23 17:55:51 -0100

    add input and buttons components to register pages

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Container } from "./styles";
+import { Button } from "../../components/Forms/Button";
+import { Input } from "../../components/Forms/Input";
+import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
+
+import {
+	Container,
+	Fields,
+	Form,
+	Header,
+	Title,
+	TransactionsType,
+} from "./styles";
 
 export const Register = () => {
-	return <Container></Container>;
+	const [transactionType, setTransactionType] = useState("");
+
+	const handleTransactionsTypeSelect = (type: "up" | "down") => {
+		setTransactionType(type);
+	};
+
+	return (
+		<Container>
+			<Header>
+				<Title>Registo</Title>
+			</Header>
+
+			<Form>
+				<Fields>
+					<Input placeholder="Nome" />
+					<Input placeholder="Preco" />
+
+					<TransactionsType>
+						<TransactionTypeButton
+							type="up"
+							title="Income"
+							isActive={transactionType === "up"}
+							onPress={() => handleTransactionsTypeSelect("up")}
+						/>
+						<TransactionTypeButton
+							type="down"
+							title="Outcome"
+							isActive={transactionType === "down"}
+							onPress={() => handleTransactionsTypeSelect("down")}
+						/>
+					</TransactionsType>
+				</Fields>
+
+				<Button title="Guardar" />
+			</Form>
+		</Container>
+	);
 };
