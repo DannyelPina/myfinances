7350c9f65fde3187cef074e93be1a9a43a2ba290
DannyelPina <nataniel06@live.com.pt>
2022-01-23 17:51:39 -0100

    create input component along with it styles and interfaces

---
+++ src/components/Forms/Input/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+import { TextInput } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export const Container = styled(TextInput)`
+	width: 100%;
+	padding: ${RFValue(12)}px;
+	font-size: ${RFValue(14)}px;
+	font-family: ${({ theme }) => theme.fonts.regular};
+
+	color: ${({ theme }) => theme.colors.title};
+	background-color: ${({ theme }) => theme.colors.shape};
+	border-radius: ${RFValue(5)}px;
+	margin-bottom: ${RFValue(8)}px;
+`;
